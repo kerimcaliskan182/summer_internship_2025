93546ec2d37da443f87c2e1027233add3ea29e1c
Yaren Amaç <yarenamac2002@gmail.com>
2025-08-26 21:32:42 +0300

    Task 2 tamamlandı

---
+++ task2.py
@@ -0,0 +1,42 @@
+#Task 2 - Power of Four
+
+""""
+def isPowerOfFour(n: int) -> bool:
+    if n <= 0:
+        return False
+    while n % 4 == 0:
+        n //= 4
+    return n == 1
+
+n = int(input("Bir sayı girin: "))
+
+if isPowerOfFour(n):
+    print("{} sayısı 4'ün kuvvetidir.".format(n))
+else:
+    print("{} sayısı 4'ün kuvveti değildir.".format(n))
+
+"""
+
+# without loops or recursion 
+
+def isPowerOfFour(n: int) -> bool:
+    # pozitif mi 
+    if n <= 0:
+        return False
+
+    # 2'nin kuvveti mi?  (tek bir bit 1 ise evet)
+    if (n & (n - 1)) != 0:     
+        return False
+
+    # 3) mod 3 = 1 mi?
+    if n % 3 != 1:
+        return False
+
+    return True
+
+n = int(input("Bir sayı girin: "))
+
+if isPowerOfFour(n):
+    print("{} sayısı 4'ün kuvvetidir.".format(n))
+else:
+    print("{} sayısı 4'ün kuvveti değildir.".format(n))
